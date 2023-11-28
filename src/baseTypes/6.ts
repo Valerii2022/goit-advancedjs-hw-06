3dd47c0e4fe4c160fecf70b01ae652c9318aeecb
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-11-29 00:21:54 +0200

    done with base types

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,24 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError() {
-  throw new Error('Error');
+function customError(): never {
+  throw new Error("Error");
 }
 
 export {};
+
+function calc2(num1: number | string, num2: number | string): number | string {
+  if (typeof num1 === "string" || typeof num2 === "string") {
+    return num1.toString() + num2.toString();
+  } else {
+    return num1 + num2;
+  }
+}
